a7ef498033e16f068b17c3f5da7e531a62356c21
Owen Adrian <akrivus@gmail.com>
2024-05-11 13:52:24 -0700

    Added Unity player.

---
+++ public/unity/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Syntreno-Demo-f1b1b3b0-1b3b-4b1b-9b1b-1b3b1b3b1b3b";
+const contentToCache = [
+    "Build/unity.loader.js",
+    "Build/unity.framework.js",
+    "Build/unity.data",
+    "Build/unity.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
